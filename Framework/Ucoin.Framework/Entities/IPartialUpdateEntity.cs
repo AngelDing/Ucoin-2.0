f1bb59b5a588cb8823223ba98948702b925bb137
jackyzhou <80103558@qq.com>
2015-01-27 09:15:41 +0800

    IPartialUpdateEntity

---
+++ Framework/Ucoin.Framework/Entities/IPartialUpdateEntity.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+namespace Ucoin.Framework.Entities
+{
+    /// <summary>
+    /// 局部更新接口
+    /// </summary>
+    public interface IPartialUpdateEntity
+    {
+        /// <summary>
+        /// 是否局部更新
+        /// </summary>
+        bool IsPartialUpdate { get; set; }
+
+        /// <summary>
+        /// 需要更新的字段及其值的集合
+        /// </summary>
+        Dictionary<string, object> NeedUpdateList { get; }
+    }
+}
