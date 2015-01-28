168edb8d63bfc4f97054c243aa95058bf035570f
jackyzhou <80103558@qq.com>
2015-01-28 10:04:25 +0800

    ServiceLocator

---
+++ Framework/Ucoin.Framework.ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,66 @@
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Configuration;
+
+namespace Ucoin.Framework.ServiceLocator
+{
+    /// <summary>
+    /// Represents the Service Locator.
+    /// </summary>
+    public sealed class ServiceLocator
+    {
+        private IUnityContainer container;
+        private static readonly ServiceLocator instance = new ServiceLocator();
+
+        /// <summary>
+        /// Initializes a new instance of <c>ServiceLocator</c> class.
+        /// </summary>
+        private ServiceLocator()
+        {
+            if (container == null)
+            {
+                var section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
+                if (section == null)
+                {
+                    return;
+                }
+                container = new UnityContainer();
+                section.Configure(container);
+            }
+        }
+
+        /// <summary>
+        /// 對於某些無配置的程序，支持代碼注入，如：
+        ///  var container = ServiceLocator.GetUnityContainer();
+        ///  if (container.IsRegistered<IOrderLogService>() == false)
+        ///  {
+        ///     container.RegisterType<IOrderLogService, OrderLogService>();
+        ///  }
+        /// </summary>
+        /// <returns></returns>
+        public static IUnityContainer GetUnityContainer()
+        {
+            instance.container = new UnityContainer();
+            return instance.container;
+        }
+       
+        /// <summary>
+        /// Gets the service instance with the given type.
+        /// </summary>
+        /// <typeparam name="T">The type of the service.</typeparam>
+        /// <returns>The service instance.</returns>
+        public static T GetService<T>()
+        {
+            try
+            {
+                return instance.container.Resolve<T>();
+            }
+            catch (Exception ex)
+            {
+                var type = typeof(T);
+                throw new ActivationException(string.Format("Service not found [{0}]", type.Name), ex);
+            }
+        }           
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -4,3 +4,24 @@ http://michaelcrump.net/setting-up-github-to-work-with-visual-studio-2013-step-b
 
 Ucoin.Framework.ObjectMapper引用開源項目：
 https://objectmapper.codeplex.com/
+
+
+1. 註冊的EfRepositoryContext，生命週期是否需要特別設定？
+
+2.Code First 
+   數據遷移：
+   手動遷移：工具 -》NuGet程序包管理其 -》程序包管理器控制台
+   a. Enable-Migrations -ProjectName XXX.YYY.ZZZ -Force
+   b. add-migration InitialCreate -ProjectName XXX.YYY.ZZZ
+      Add-Migration FirstMigration -ProjectName XXX.YYY.ZZZ
+   c. Update-Database -ProjectName XXX.YYY.ZZZ
+   自動遷移：在Migrations.Configuration.cs中，修改AutomaticMigrationsEnabled = true;
+   另外在程序啟動時調用 UcoinDBContextInitailizer.Initialize();并禁用app.config中的<contexts>配置項；
+   
+   重要提示：配置的鏈接字符串中，name="數據庫實例的名稱";否則不管自動還是手動遷移均會碰到很多問題；
+
+3. 對象轉換性能： 手動 》 EmitMapper 》 ObjectMapper 》 AutoMapper
+
+4. Ucoin.Framework.ServiceLocator爲什麽要獨立成一個項目？
+   -》之前也想放在Ucoin.Framework項目中，但是默認實現是基於Unity的，這樣Ucoin.Framework需要引用第三的Unity組件，爲了保持Ucoin.Framework
+   的公用性，最好不要引用第三方組件，如果ServiceLocator實現依賴于其他的IOC組件，則需要添加更多的第三方組件，故獨立出一個單獨的項目。
