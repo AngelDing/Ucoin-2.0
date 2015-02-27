b2bd412e8f0f74efb3f15e7e1f424b0e4425884f
jackyzhou <80103558@qq.com>
2015-02-27 14:50:12 +0800

    LogLevelType

---
+++ Framework/Ucoin.Framework.Log/LogLevelType.cs
@@ -0,0 +1,22 @@
+namespace Ucoin.Framework.Logging
+{
+    public enum LogLevelType
+    {
+        All   = 0,
+
+        Trace = 1,
+
+        Debug = 2,
+
+        Info  = 3,
+
+        Warn  = 4,
+
+        Error = 5,
+
+        Fatal = 6,
+
+        Off  = 7
+
+    }  
+}
